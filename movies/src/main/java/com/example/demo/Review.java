c673420262ccdfec6b7e546cebe3d2c89730dee2
c_du <102283742+charlottedu1031@users.noreply.github.com>
2024-05-02 00:33:58 -0400

    Set up .env and created 2 .java files

---
+++ movies/src/main/java/com/example/demo/Review.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    private ObjectId id;
+    private String body;
+}
