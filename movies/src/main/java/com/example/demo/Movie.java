77fdb82c3829a32c653e2b2c08beaede373f1a0b
c_du <102283742+charlottedu1031@users.noreply.github.com>
2024-05-02 18:24:23 -0400

    Created repo, controller and services java files

--- movies/src/main/java/com/example/demo/Movie.java
+++ movies/src/main/java/com/example/demo/Movie.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class movie {
+public class Movie {
     @Id
     private ObjectId id;
     private String imdbId;
--- movies/src/main/java/com/example/demo/Review.java
+++ movies/src/main/java/com/example/demo/Review.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "reviews")
@@ -12,6 +13,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Review {
+    @Id
     private ObjectId id;
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
